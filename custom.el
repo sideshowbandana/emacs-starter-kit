4df225e0f6b9fb3203105cb4aad4d3bda83d14e7
Kyle Barton <kyle.barton@mylookout.com>
2011-07-25 09:59:08 -0700

    Removed custom.el from .gitignore and checked it in

---
+++ custom.el
@@ -0,0 +1,17 @@
+(color-theme-blackboard)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(hl-line-sticky-flag nil))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "#FF4444" :slant italic))))
+ '(font-lock-function-name-face ((t (:foreground "#8DD9CE"))))
+ '(font-lock-variable-name-face ((t (:foreground "#e8ff33"))))
+ '(magit-item-highlight ((((class color) (background dark)) nil))))
